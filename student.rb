b464382c10ad6b9dbfc1a1f52b6daadb8cefbdb3
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-13 10:44:56 +0100

    Fix linters error

--- student.rb
+++ student.rb
@@ -1,7 +1,8 @@
 require './person'
 
 class Student < Person
-   attr_reader :classroom
+  attr_reader :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
