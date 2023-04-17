05abb124e14a1fe46994d6e1618777764960cfda
Okolo Raphael <okolonnaemeka2017@gmail.com>
2023-04-17 13:09:37 +0100

    Catch a bug with pry

--- student.rb
+++ student.rb
@@ -1,15 +1,13 @@
 require './person'
-
 class Student < Person
   attr_reader :classroom
 
-  def initialize(age, name, parent_permission)
+  def initialize( age, name, parent_permission)
     super(name)
     @age = age
     @parent_permission = parent_permission
     @name = name
   end
-
   def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
